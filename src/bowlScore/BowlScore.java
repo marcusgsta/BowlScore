47527334792bf5d1c55aea1fb58a28de837f8d6b
Marcus Gustafsson <marcusgu@hotmail.com>
2018-05-12 16:27:44 +0200

    reconstructed to handle frames

--- src/bowlScore/BowlScore.java
+++ src/bowlScore/BowlScore.java
@@ -1,5 +1,6 @@
 package bowlScore;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class BowlScore {
@@ -12,27 +13,48 @@ public class BowlScore {
 	public Object registerScore(int[] input) {
 		int totalSum;
 		int partSum = 0;
-		int strike = 0;
+		boolean strike = false;
+		boolean spare = false;
 		int savedPoints = 0;
+		int currentFrame = 0;
+		int lastFrame = currentFrame - 1;
 		int[] frames = new int[10];
+		int count = 1;
+		int partScore = 0;
+		
 		for (int i = 0; i < input.length; i++) {
 			if (input[i] >= 0 || input[i] <= 10)
+				partScore += input[i];
+				if ( count == 2)
+					frames[currentFrame] += partScore;
+					currentFrame++;
+					partScore = 0;
+				count++;
+			
+				/*
 				partSum += input[i];
-				frames[0] += input[i];
+				if (count == 2)
+					frames[frameNr] += ( input[i] + input[i-1] );
+				frames[frameNr] += input[i];
 				
-				//if (strike == 1)
-					
+				// if (input[i] == 10)
+					//strike = true;
+				if ( input[i] > 0 )
+					if ( input[i-1] + input[i] == 10 )
+						spare = true;
+				if ( spare == true )
+					frames[lastFrame] += input[i];
 				
-				//if (input[i] == 10)
-				//	strike += 1;
-				//	frames[0] += 10;
+			count++;
+			*/
 		}
 		
 		int sum = IntStream.of(frames).sum();
+		System.out.println(Arrays.toString(frames));
 
 		System.out.println(sum);
-		totalSum = partSum;
-		return totalSum;
+		//totalSum = partSum;
+		return sum;
 	}
 
 
