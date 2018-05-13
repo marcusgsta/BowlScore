b4029566892b71d292d2f5f1b844d3105db0272a
Marcus Gustafsson <marcusgu@hotmail.com>
2018-05-13 21:27:06 +0200

    implemented user stories

--- src/bowlScore/BowlScore.java
+++ src/bowlScore/BowlScore.java
@@ -19,30 +19,51 @@ public class BowlScore {
 		int totalScore = 0;
 		int strikeBonus = 0;
 		int spareBonus = 0;
+		boolean strikeStep = false;
 		
 		for (int i = 0; i < input.length; i++) {
 			if (input[i] >= 0 || input[i] <= 10)
 				partScore += input[i];
 			
 				if ( count == 1 ) {
+					if ( strikeStep == true ) {
+						strikeBonus += input[i];
+						strikeStep = false;
+					}
 					if ( strike == 1 ) {
 						strikeBonus += input[i];
-					} else if ( isSpare == true ) {
+						strike = 0;
+						strikeStep = true;
+					}
+					/*else if ( strike == 2 ) {
+						strikeBonus += input[i];
+						strike -= 1;
+					} */
+					else if ( isSpare == true ) {
 						spareBonus += input[i];
 						isSpare = false;
 					}
+
 					if ( input[i] == 10 ) {
 						strike += 1;
+						totalScore += partScore;
+						partScore = 0;
 						count = 0;
 					}
 				}
 				
 				if ( count == 2 ) {
+					if ( strikeStep == true ) {
+						strikeBonus += input[i];
+						strikeStep = false;
+					}
 					if ( partScore == 10 ) {
 						isSpare = true;
-					} else if ( strike == 1 ) {
-						strikeBonus += input[i];
-						strike -= 1;
+					} else if ( partScore != 10) {
+						if ( strike == 1 ) {
+							strikeBonus += input[i];
+							strike--;
+						}
 					}
 					totalScore += partScore;
 					partScore = 0;
