4eb9cc66465e4ad9f55bb76c89fd1485d9b43b4c
Marcus Gustafsson <marcusgu@hotmail.com>
2018-05-15 15:42:09 +0200

    Cleaned up

--- src/bowlScore/BowlScore.java
+++ src/bowlScore/BowlScore.java
@@ -6,18 +6,12 @@ public class BowlScore {
 
 	public Object registerScore(int[][] input) {
 		int strike = 0;
-		int[] frames = new int[10];
-		int currentFrame = 0;
-		int ball = 1;
 		int partScore = 0;
 		boolean isSpare = false;
 		int totalScore = 0;
 		int strikeBonus = 0;
 		int spareBonus = 0;
 		boolean strikeStep = false;
-		boolean lastFrame = false;
-		int count;
-		int[] game = new int[10];
 		
 		if (input.length != 10) {
 			throw new IndexOutOfBoundsException("Index is out of bounds!");
@@ -61,20 +55,7 @@ public class BowlScore {
 				if ( input[m][h] == 10 && Arrays.asList(input).indexOf(input[m]) != 9) {
 					strike += 1;
 					break;
-					
-					//frames[currentFrame] = totalScore;
-					/*currentFrame++;
-					if ( currentFrame == 10) {
-						lastFrame = true;
-					}*/
-					// if strike in last frame
-				/*} else if ( input[m][h] == 10 && lastFrame == true) {
-						ball = 0;
-				}*/
-				
 				}
-
-				//System.out.println(Arrays.asList(input).indexOf(input[m]));
 				
 				if ( partScore == 10 && Arrays.asList(input).indexOf(input[m]) != 9) {
 					isSpare = true;
@@ -84,69 +65,8 @@ public class BowlScore {
 			totalScore += partScore;
 			partScore = 0;	
 		}
-		/*
-		for (int i = 0; i < input.length; i++) {
-			if (input[i] >= 0 || input[i] <= 10)
-				partScore += input[i];
-			
-				if ( ball == 1 ) {
-					if ( strikeStep == true ) {
-						strikeBonus += input[i];
-						strikeStep = false;
-					}
-					if ( strike == 1 ) {
-						strikeBonus += input[i];
-						strike = 0;
-						strikeStep = true;
-					} else if ( isSpare == true ) {
-						spareBonus += input[i];
-						isSpare = false;
-					}
-
-					if ( input[i] == 10 && lastFrame == false) {
-						strike += 1;
-						totalScore += partScore;
-						partScore = 0;
-						ball = 0;
-						frames[currentFrame] = totalScore;
-						currentFrame++;
-						if ( currentFrame == 10) {
-							lastFrame = true;
-						}
-						// if strike in last frame
-					} else if ( input[i] == 10 && lastFrame == true) {
-							ball = 0;
-					}
-				}
-				
-				
-				if ( ball == 2 ) {
-					if ( strikeStep == true ) {
-						strikeBonus += input[i];
-						strikeStep = false;
-					}
-					if ( partScore == 10 ) {
-						isSpare = true;
-					} else if ( partScore != 10) {
-						if ( strike == 1 ) {
-							strikeBonus += input[i];
-							strike--;
-						}
-					}
-					totalScore += partScore;
-					partScore = 0;
-					ball = 0;
-					frames[currentFrame] = totalScore;
-					currentFrame++;
-				} 
-				
-				ball++;
-				
-		}
-		*/
 		totalScore += spareBonus;
 		totalScore += strikeBonus;
-		//int totalSum = totalScore;
 		return totalScore;
 	}
 }
