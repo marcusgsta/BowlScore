a3ba2f84a568187a21ac170c413c36faea092b11
Marcus Gustafsson <marcusgu@hotmail.com>
2018-05-15 16:10:56 +0200

    added more comments

--- src/bowlScore/BowlScore.java
+++ src/bowlScore/BowlScore.java
@@ -12,7 +12,7 @@ public class BowlScore {
 		int strikeBonus = 0;
 		int spareBonus = 0;
 		boolean strikeStep = false;
-		
+		// Number of frames must be 10
 		if (input.length != 10) {
 			throw new IndexOutOfBoundsException("Index is out of bounds!");
 		}
@@ -20,6 +20,7 @@ public class BowlScore {
 		for (int m = 0; m < input.length; m++) {
 			// loop through frame
 			
+			// if length of a frame is not 2, it has to begin with either a strike or a spare
 			for ( int h = 0; h < input[m].length; h++ ) {
 				// if length of a frame is not 2
 				if ( input[m].length != 2 ) {
@@ -37,26 +38,28 @@ public class BowlScore {
 				// for all valid balls, add the regular points
 				if (input[m][h] >= 0 || input[m][h] <= 10)
 					partScore += input[m][h];
-				
+				// check for second ball after a strike "strikeStep"
 				if ( strikeStep == true ) {
 					strikeBonus += input[m][h];
 					strikeStep = false;
 				}
+				// check for strike
 				if ( strike == 1 ) {
 					strikeBonus += input[m][h];
 					strike = 0;
 					strikeStep = true;
+					// check for spare
 				} else if ( isSpare == true ) {
 					spareBonus += input[m][h];
 					isSpare = false;
 				}
 				
-				// if strike
+				// if strike, remember this for the next throws, unless in last frame
 				if ( input[m][h] == 10 && Arrays.asList(input).indexOf(input[m]) != 9) {
 					strike += 1;
 					break;
 				}
-				
+				// if spare, remember this unless in last frame
 				if ( partScore == 10 && Arrays.asList(input).indexOf(input[m]) != 9) {
 					isSpare = true;
 				}
@@ -65,6 +68,7 @@ public class BowlScore {
 			totalScore += partScore;
 			partScore = 0;	
 		}
+		// add bonuses for spares and strike to total
 		totalScore += spareBonus;
 		totalScore += strikeBonus;
 		return totalScore;
